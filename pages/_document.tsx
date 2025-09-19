0361e62d21a33717bb2dc5e595611856f23d683d
TyHen88 <henthevath@gmail.com>
2025-09-20 00:06:32 +0700

    fix bug

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" type="image/svg+xml" href="/planpro-favicon.svg" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        {/* Favicon configuration - order matters */}
+        <link rel="icon" type="image/svg+xml" href="/asset/planpro-favicon.svg" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/asset/planpro-favicon.svg" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/asset/planpro-favicon.svg" />
+        <link rel="shortcut icon" href="/asset/planpro-favicon.svg" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/asset/planpro-icon.svg" />
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Override Next.js default favicon */}
+        <link rel="icon" href="/asset/planpro-favicon.svg" />
+
         <meta name="theme-color" content="#3B82F6" />
         <meta name="description" content="PlanPro - Smart Project Management System" />
       </Head>
